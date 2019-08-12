118a946176d67520142aa96aca47aa9b9d28ed54
mattHawthorn <hawthorn.matthew@gmail.com>
2019-08-12 01:56:02 -0400

    Bump version: 0.1.0 → 0.1.1

--- bourbaki/iterutils/__init__.py
+++ bourbaki/iterutils/__init__.py
@@ -2,4 +2,4 @@
 from .iterutils import batched, even_batched, peek, check_empty_iter, zipcall, filltail, do_all
 from .classes import Sliceable, to_sliceable
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
